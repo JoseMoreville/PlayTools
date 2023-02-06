da12458e916e96ec3fe5a35d80a15be788110c50
José Moreno <47700212+JoseMoreville@users.noreply.github.com>
2023-02-06 00:25:23 -0700

    more floating error and macOS fixes

--- PlayTools/PlayScreen.swift
+++ PlayTools/PlayScreen.swift
@@ -20,7 +20,7 @@ extension CGSize {
     }
 
     func toAspectRatio() -> CGSize {
-        if macOSVersion >= 13.199 {
+        if macOSVersion >= 13.19000 {
             return CGSize(width: mainScreenWidth, height: mainScreenHeight)
         } else {
             return CGSize(width: mainScreenHeight, height: mainScreenWidth)
