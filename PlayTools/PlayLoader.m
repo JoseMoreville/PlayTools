3c41a2cb56a70d6a05189993cb30e86ae8022fac
José Moreno <47700212+JoseMoreville@users.noreply.github.com>
2023-02-05 23:51:54 -0700

    more

--- PlayTools/PlayLoader.m
+++ PlayTools/PlayLoader.m
@@ -146,6 +146,13 @@ static OSStatus pt_SecItemDelete(CFDictionaryRef query) {
         retval = SecItemDelete(query);
     }
     [PlayKeychain debugLogger: [NSString stringWithFormat:@"SecItemDelete: %@", query]];
+    if ([[PlaySettings shared] macOSVersion] > 13.1) {
+        NSLog(@"macOSVersion es mayor o igual a 13.1");
+        NSLog(@"macOS version: %f", [[PlaySettings shared] macOSVersion]);
+    } else {
+        NSLog(@"macOSVersion es menor a 13.1");
+        NSLog(@"macOS version: %f", [[PlaySettings shared] macOSVersion]);
+    }
     return retval;
 }
 
