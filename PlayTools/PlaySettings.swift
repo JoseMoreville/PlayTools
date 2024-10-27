30722f20fe3a7ae9026f6d1857b5421904cac638
José Moreno <47700212+JoseMoreville@users.noreply.github.com>
2024-10-27 00:04:02 -0700

    sdfs

--- PlayTools/PlaySettings.swift
+++ PlayTools/PlaySettings.swift
@@ -105,5 +105,5 @@ struct AppSettingsData: Codable {
     var rootWorkDir = true
     var noKMOnInput = false
     var enableScrollWheel = true
-    var autoResize = false
+    var autoResize = true
 }
